a49dfda60a00c42408392fa552d6c2440ff2357f
Djair Silva <djavasilva@yahoo.com.br>
2024-07-17 12:04:02 -0300

    2 subnet ALB OK

--- alb.tf
+++ alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.subnet1.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   enable_deletion_protection = false
 
--- subnet.tf
+++ subnet.tf
@@ -16,19 +16,19 @@ resource "aws_route_table_association" "subnet1_association" {
   route_table_id = aws_vpc.vpc2.main_route_table_id
 }
 
-# Exemplo de criação de uma segunda subnet (descomentando as linhas abaixo)
-# resource "aws_subnet" "subnet2" {
-#   vpc_id            = aws_vpc.vpc2.id
-#   cidr_block        = "172.0.2.0/24"
-#   availability_zone = "us-east-1b"
-#   tags = {
-#     Name     = "${var.tag_name}-2"
-#     App      = var.tag_app
-#     Servico  = var.tag_servico
-#   }
-# }
+#Exemplo de criação de uma segunda subnet (descomentando as linhas abaixo)
+resource "aws_subnet" "subnet2" {
+   vpc_id            = aws_vpc.vpc2.id
+   cidr_block        = "172.0.2.0/24"
+   availability_zone = "us-east-1b"
+   tags = {
+     Name     = "${var.tag_name}-2"
+     App      = var.tag_app
+     Servico  = var.tag_servico
+   }
+ }
 
-# resource "aws_route_table_association" "subnet2_association" {
-#   subnet_id      = aws_subnet.subnet2.id
-#   route_table_id = aws_vpc.vpc2.main_route_table_id
-# }
+ resource "aws_route_table_association" "subnet2_association" {
+   subnet_id      = aws_subnet.subnet2.id
+   route_table_id = aws_vpc.vpc2.main_route_table_id
+ }
